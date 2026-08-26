8075f806059fbf8c0ff5ca31850f5c6385fcd8ce
[Cole Northway] <[cenorthway289@gmail.com]>
2026-08-26 02:23:58 -0500

    Write Game Plan

--- lib/connect4.rb
+++ lib/connect4.rb
@@ -1,2 +1,15 @@
 # frozen_string_literal: true
 
+=begin
+  0) Read over the assignment requirements
+  1) First lets establish our classes and their roles
+      + UI Class
+        - Handles all printing and prompting
+      + Board Class
+        - Handles the game logic
+        - Tracks the board state
+        - Determines win or draw
+      + Controller Class
+        - Handles game start, turns and game end
+        - Loops
+=end
