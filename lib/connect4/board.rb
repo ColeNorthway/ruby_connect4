ef747e9fb7f2cfb3752969fd914d66ec8a90e09d
[Cole Northway] <[cenorthway289@gmail.com]>
2026-08-27 16:08:16 -0500

    Get Veritical and Horizonal Checks Completed

--- lib/connect4/board.rb
+++ lib/connect4/board.rb
@@ -2,12 +2,15 @@
 
 require_relative 'player'
 
+# Board represents the game state
+#   - Can decide winners
+#   - Tracks state as grid
 class Board
   attr_reader :coordinates
 
   def initialize
     @coordinates = []
-    @grid = Array.new(6) { Array.new(7) }
+    @grid = Array.new(7) { Array.new(6) }
   end
 
   def submit(player, coord)
@@ -15,12 +18,12 @@ class Board
     coords = coord.split(',')
     x = coords[0].to_i
     y = coords[1].to_i
-    @grid[x][y] = player.char
+    @grid[x - 1][y - 1] = player.char
   end
 
-  def won?
-    # Call internal for horizontal
-    # Call internal for vertical
+  def won?(player)
+    return true if self.horizontal_win?(player)
+    return true if self.vertical_win?(player)
     # Call internal for diagonal
   end
 
@@ -29,15 +32,41 @@ class Board
   attr_accessor :grid
   attr_writer :coordinates
 
-  def horizontal_win?
+  def horizontal_win?(player)
+    @grid[0].length.times do |i|
+      horiz_arr = @grid.reduce([]) { |h_arr, v_arr| h_arr.push(v_arr[i]) }
 
+      begin
+        case horiz_arr
+        in [*, ^(player.char), ^(player.char), ^(player.char), ^(player.char), *]
+          return true
+        end
+      rescue NoMatchingPatternError
+      end
+    end
+
+    false
   end
 
-  def vertical_win?
+  def vertical_win?(player)
+    @grid.each do |col|
+      begin
+        case col
+        in [*, ^(player.char), ^(player.char), ^(player.char), ^(player.char), *]
+          return true
+        end
+      rescue NoMatchingPatternError
+      end
+    end
 
+
+    false
   end
 
-  def diagonal_win?
-
+  def diagonal_win?(player)
+    # Get it working dirty first
+    # Iterate the grid for each vert arr
+    # For each sub_indx of each vert arr
+    #   - Do an ascending if indx <=2 (zero-based)
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,41 +8,51 @@ RSpec.describe Board do
     it 'Four in a row horizontal wins' do
       board = Board.new
       player = Player.new("\u{1F534}", 'rupert')
-      board.submit(player, '1,1')
-      board.submit(player, '2,1')
-      board.submit(player, '3,1')
-      board.submit(player, '4,1')
-      expect(board.won?).to eql(true)
+      board.submit(player, '1,3')
+      board.submit(player, '2,3')
+      board.submit(player, '3,3')
+      board.submit(player, '4,3')
+      expect(board.won?(player)).to eql(true)
     end
 
     it 'Four in a row vertical wins' do
       board = Board.new
       player = Player.new("\u{1F534}", 'rupert')
-      board.submit(player, '1,1')
-      board.submit(player, '1,2')
-      board.submit(player, '1,3')
-      board.submit(player, '1,4')
-      expect(board.won?).to eql(true)
+      board.submit(player, '2,1')
+      board.submit(player, '2,2')
+      board.submit(player, '2,3')
+      board.submit(player, '2,4')
+      expect(board.won?(player)).to eql(true)
     end
 
-    it 'Four in a row diagnal wins' do
+    it 'Four in a row - forward diagnal wins' do
       board = Board.new
       player = Player.new("\u{1F534}", 'rupert')
       board.submit(player, '1,1')
       board.submit(player, '2,2')
       board.submit(player, '3,3')
       board.submit(player, '4,4')
-      expect(board.won?).to eql(true)
+      expect(board.won?(player)).to eql(true)
     end
 
-    it "None of those doesn't win" do
+    it 'Four in a row - reverse diagnal wins' do
+      board = Board.new
+      player = Player.new("\u{1F534}", 'rupert')
+      board.submit(player, '1,4')
+      board.submit(player, '2,3')
+      board.submit(player, '3,2')
+      board.submit(player, '4,1')
+      expect(board.won?(player)).to eql(true)
+    end
+
+    it "None of those win" do
       board = Board.new
       player = Player.new("\u{1F534}", 'rupert')
       board.submit(player, '1,1')
       board.submit(player, '2,2')
       board.submit(player, '3,3')
       board.submit(player, '4,3')
-      expect(board.won?).to eql(false)
+      expect(board.won?(player)).to eql(false)
     end
   end
 end
